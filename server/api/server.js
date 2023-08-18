ae1cac8e969ef8ab73da20b38251dc8ebeba5e90
Victor Luna <victor@accounttech.com>
2023-08-17 21:07:37 -0500

    update filepath db.json

--- server/api/server.js
+++ server/api/server.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("./db.json");
 
 server.use(router);
 server.listen(3000, () => {
