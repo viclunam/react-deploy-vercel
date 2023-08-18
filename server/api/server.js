7a48859d5e3ef15fa0ab6436915b23189479bcf7
Victor Luna <victor@accounttech.com>
2023-08-17 21:27:04 -0500

    update filepath db.json path join with server

--- server/api/server.js
+++ server/api/server.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const dbPath = path.join(process.cwd(), "db.json");
+const dbPath = path.join(process.cwd(), "server", "db.json");
 const router = jsonServer.router(dbPath);
 
 server.use(router);
