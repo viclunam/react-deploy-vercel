3b6d1c42cca2b293ded8316d5cebf6a1c0332af2
Victor Luna <victor@accounttech.com>
2023-08-17 21:39:40 -0500

    server: add cors

--- server/api/server.js
+++ server/api/server.js
@@ -1,9 +1,11 @@
 const path = require("path");
+const cors = require("cors");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const dbPath = path.join(process.cwd(), "server", "db.json");
 const router = jsonServer.router(dbPath);
 
+server.use(cors());
 server.use(router);
 server.listen(3000, () => {
   console.log("JSON Server is running");
